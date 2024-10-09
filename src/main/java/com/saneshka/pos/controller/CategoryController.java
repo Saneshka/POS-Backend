8d396c9206025cd1dc899530218c3136007d8172
Saneshka Costa <costasaneshka@outlook.com>
2024-10-09 05:48:40 +0530

    allow cross origins on category controller

--- src/main/java/com/saneshka/pos/controller/CategoryController.java
+++ src/main/java/com/saneshka/pos/controller/CategoryController.java
@@ -2,6 +2,7 @@ package com.saneshka.pos.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -12,6 +13,7 @@ import com.saneshka.pos.service.CategoryService;
 import java.util.List;
 
 @RestController
+@CrossOrigin(origins = "*")
 public class CategoryController {
 
     @Autowired
