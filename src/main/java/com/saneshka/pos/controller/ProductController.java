2881fcd447e07f6d4f03be1249cc28573e59a24b
Saneshka Costa <costasaneshka@outlook.com>
2024-10-12 18:11:26 +0530

    allow cross origins in product controller

--- src/main/java/com/saneshka/pos/controller/ProductController.java
+++ src/main/java/com/saneshka/pos/controller/ProductController.java
@@ -2,6 +2,7 @@ package com.saneshka.pos.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -17,6 +18,7 @@ import com.saneshka.pos.service.ProductService;
 import java.util.List;
 
 @RestController
+@CrossOrigin(origins = "*")
 public class ProductController {
     @Autowired
     private ProductService productService;
