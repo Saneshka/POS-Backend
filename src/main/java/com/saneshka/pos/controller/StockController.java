a7b82dace68cdf88749d3315a4806c364edca41a
Saneshka Costa <costasaneshka@outlook.com>
2024-10-19 17:58:45 +0530

    allow cross origins in stock

--- src/main/java/com/saneshka/pos/controller/StockController.java
+++ src/main/java/com/saneshka/pos/controller/StockController.java
@@ -3,6 +3,7 @@ package com.saneshka.pos.controller;
 import java.util.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -18,6 +19,7 @@ import com.saneshka.pos.service.ProductService;
 import com.saneshka.pos.service.StockService;
 
 @RestController
+@CrossOrigin(origins = "*")
 public class StockController {
     @Autowired
     private StockService stockService;
