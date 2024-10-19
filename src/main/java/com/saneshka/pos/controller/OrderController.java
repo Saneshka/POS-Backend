7ce95d813c044856a46c211edff4a9abec7ce9d5
Saneshka Costa <costasaneshka@outlook.com>
2024-10-19 19:29:51 +0530

    Update OrderController to reduce the stock when add new orders

--- src/main/java/com/saneshka/pos/controller/OrderController.java
+++ src/main/java/com/saneshka/pos/controller/OrderController.java
@@ -10,12 +10,15 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 import java.util.ArrayList;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.saneshka.pos.dto.OrderDTO;
 import com.saneshka.pos.entity.Order;
 import com.saneshka.pos.entity.Product;
+import com.saneshka.pos.entity.Stock;
 import com.saneshka.pos.service.OrderService;
 import com.saneshka.pos.service.ProductService;
+import com.saneshka.pos.service.StockService;
 
 @RestController
 @CrossOrigin("*")
@@ -23,6 +26,9 @@ public class OrderController {
 
     @Autowired
     private OrderService orderService;
+
+    @Autowired
+    private StockService stockService;
 
     @Autowired
     private ProductService productService;
@@ -35,34 +41,55 @@ public class OrderController {
     }
 
     @PostMapping("/orders")
+    @Transactional
     public ResponseEntity<Order> createOrder(@RequestBody OrderDTO orderDTO) {
-
         try {
+
             Order order = new Order();
             order.setTotalPrice(0.0);
             List<Product> orderedProducts = new ArrayList<>();
 
             List<Long> productIds = orderDTO.getProductIds();
-            productIds.forEach(productId -> {
-
+            for (Long productId : productIds) {
                 Product product = productService.getProductById(productId);
 
                 if (product != null) {
                     orderedProducts.add(product);
-                    order.setTotalPrice(order.getTotalPrice() + product.getPrice());
-                }
 
-            });
+                    Stock newStock = new Stock();
+                    Stock existingStock = product.getStock();
+
+                    Integer existingQty = existingStock.getQty();
+                    if (existingQty <= 0) {
+                        throw new RuntimeException("Insufficient stock for product: " + product.getProductName());
+                    }
+
+                    Integer newQty = existingQty - 1;
+
+                    newStock.setId(existingStock.getId());
+                    newStock.setQty(newQty);
+                    newStock.setProduct(product);
+
+                    stockService.updateStock(existingStock.getId(), newStock);
+
+                    order.setTotalPrice(order.getTotalPrice() + product.getPrice());
+                } else {
+                    throw new RuntimeException("Product with ID " + productId + " not found.");
+                }
+            }
 
             order.setOrderedProducts(orderedProducts);
-
             orderService.createOrder(order);
 
             return ResponseEntity.status(201).body(order);
 
-        } catch (Exception e) {
-            return ResponseEntity.status(400).body(null);
-            // TODO: handle exception
+        } catch (RuntimeException e) {
+
+            System.err.println("Error creating order: " + e.getMessage());
+
+            // return ResponseEntity.status(400).body(null);
+            throw e;
         }
     }
+
 }
