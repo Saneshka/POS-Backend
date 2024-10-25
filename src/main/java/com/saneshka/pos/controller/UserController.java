267dd152918fdc7bf1253c56fef5a40c55b0d04b
Saneshka Costa <costasaneshka@outlook.com>
2024-10-25 20:51:00 +0530

    Add the method to update user in controller

--- src/main/java/com/saneshka/pos/controller/UserController.java
+++ src/main/java/com/saneshka/pos/controller/UserController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -34,7 +35,16 @@ public class UserController {
             User createdUser = userService.createUser(user);
             return ResponseEntity.status(201).body(createdUser);
         } catch (Exception e) {
-            // TODO: handle exception
+            return ResponseEntity.status(400).body(null);
+        }
+    }
+
+    @PutMapping("users/{id}")
+    public ResponseEntity<User> updateUser(@PathVariable Long id, @RequestBody User user) {
+        try {
+            User updatedUser = userService.updateUser(id, user);
+            return ResponseEntity.status(200).body(updatedUser);
+        } catch (Exception e) {
             return ResponseEntity.status(400).body(null);
         }
     }
